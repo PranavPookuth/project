0a1fa91fdb45c509e50990e41bc195c230cd72c6
PranavPookuth <135806603+PranavPookuth@users.noreply.github.com>
2023-08-09 21:00:28 +0530

    Add files via upload

---
+++ LARGEST AMONG THRESS NUMBER.py
@@ -0,0 +1,11 @@
+a=float(input("A:"))
+b=float(input("B:"))
+c=float(input("C:"))
+if a>b and a>c:
+    print("larger is A")
+elif b>a and b>c:
+    print("larger is B")
+else:
+    print("larger is C")
+
+
---
+++ swapping.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number : "))
+b=int(input("Enter the second number : "))
+print("Value before swapping A = ",a)
+print("Value before swapping B = ",b)
+c=a
+a=b
+b=c
+print("Value after swapping A =",a)
+print("Value after swapping B =",b)
